91345e302b689fc8a998d9c26335d08b7306c129
Denys Harshanov <94528479+dflymegold@users.noreply.github.com>
2022-03-01 18:02:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,43 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
-from dash import dcc, html, Input, Output
+from get_plots import get_scatter_plot
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
-
-app.layout = html.Div([
-    dcc.Slider(0, 20, 5,
-               value=10,
-               id='my-slider'
-    ),
-    html.Div(id='slider-output-container')
+app.layout = html.Div(children=[
+       html.Div([
+           dcc.Graph(id='fig1'),
+       ]) ,
+       html.Div([
+           html.H6('Time period (days)'),
+           dcc.Slider(
+               id='slider-day1',
+               min=0,
+               max=100,
+               step=1,
+               value=30,
+               marks={i: str(i) for i in range(0, 100, 10)}
+           ),
+           html.H6('Number of breweries from the top'),
+           dcc.Slider(
+               id='slider-top1',
+               min=0,
+               max=500,
+               step=50,
+               value=500,
+               marks={i: str(i) for i in range(0, 500, 50)})
+       ])
 ])
 
-@app.callback(
-    Output('slider-output-container', 'children'),
-    Input('my-slider', 'value'))
-def update_output(value):
-    return 'You have selected "{}"'.format(value)
 
+@app.callback(
+   dash.dependencies.Output('fig1', 'figure'),
+   [dash.dependencies.Input('slider-day1', 'value'),
+    dash.dependencies.Input('slider-top1', 'value')])
+def output_fig(n_days, top_n):
+    get_scatter_plot(n_days, top_n)
+    
 if __name__ == '__main__':
     app.run_server(debug=True)
