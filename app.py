7ef255c9ea70f792fdc40733be810687267c99b0
Denys Harshanov <94528479+dflymegold@users.noreply.github.com>
2022-03-01 23:19:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,42 @@ githublink='https://github.com/dflymegold/availability-graph'
 function = go.Figure(data=[go.Scatter(x=t, y=np.sin(t))])
 
 
+fig = go.Figure()
+
+for step in np.arange(0, 5, 0.1):
+    fig.add_trace(
+        go.Scatter(
+            visible=False,
+            line=dict(color="#00CED1", width=6),
+            name="𝜈 = " + str(step),
+            x=np.arange(0, 10, 0.01),
+            y=np.sin(step * np.arange(0, 10, 0.01))))
+
+    
+fig.data[10].visible = True
+
+
+steps = []
+for i in range(len(fig.data)):
+    step = dict(
+        method="update",
+        args=[{"visible": [False] * len(fig.data)},
+              {"title": "Slider switched to step: " + str(i)}],  # layout attribute
+    )
+    step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
+    steps.append(step)
+
+sliders = [dict(
+    active=10,
+    currentvalue={"prefix": "Frequency: "},
+    pad={"t": 50},
+    steps=steps
+)]
+
+fig.update_layout(
+    sliders=sliders
+)
+
 
 
 
@@ -35,7 +71,7 @@ app.layout = html.Div(children=[
     html.H1(myheading),
     dcc.Graph(
         id='graph',
-        figure=function ),
+        figure=fig ),
         html.Div([
          html.H6(label1),
          dcc.Slider(
