70ca4033cef1dead3f13470acd448025648495fe
Denys Harshanov <94528479+dflymegold@users.noreply.github.com>
2022-03-01 18:38:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,62 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from get_plots import get_scatter_plot
+import plotly.graph_objs as go
+import numpy as np
 
+
+########### Define your variables
+beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+ibu_values=[35, 60, 85, 75]
+abv_values=[5.4, 7.1, 9.2, 4.3]
+color1='darkred'
+color2='orange'
+mytitle='Beer Comparison'
+tabtitle='beer!'
+myheading='Flying Dog Beers'
+label1='IBU'
+label2='ABV'
+githublink='https://github.com/austinlasseter/flying-dog-beers'
+sourceurl='https://www.flyingdog.com/beers/'
+
+########### Set up the chart
+bitterness = go.Bar(
+    x=beers,
+    y=ibu_values,
+    name=label1,
+    marker={'color':color1}
+)
+alcohol = go.Bar(
+    x=beers,
+    y=abv_values,
+    name=label2,
+    marker={'color':color2}
+)
+
+beer_data = [bitterness, alcohol]
+beer_layout = go.Layout(
+    barmode='group',
+    title = mytitle
+)
+
+beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+
+
+########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title=tabtitle
 
+########### Set up the layout
 app.layout = html.Div(children=[
-       html.Div([
-           dcc.Graph(id='fig1'),
-       ]) ,
-       html.Div([
-           html.H6('Time period (days)'),
-           dcc.Slider(
-               id='slider-day1',
-               min=0,
-               max=100,
-               step=1,
-               value=30,
-               marks={i: str(i) for i in range(0, 100, 10)}
-           ),
-           html.H6('Number of breweries from the top'),
-           dcc.Slider(
-               id='slider-top1',
-               min=0,
-               max=500,
-               step=50,
-               value=500,
-               marks={i: str(i) for i in range(0, 500, 50)})
-       ])
-])
+    html.H1(myheading),
+    dcc.Graph(
+        id='flyingdog',
+        figure=beer_fig
+    )
+    ]
+)
 
-
-@app.callback(
-   dash.dependencies.Output('fig1', 'figure'),
-   [dash.dependencies.Input('slider-day1', 'value'),
-    dash.dependencies.Input('slider-top1', 'value')])
-def output_fig(n_days, top_n):
-    get_scatter_plot(n_days, top_n)
-    
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
