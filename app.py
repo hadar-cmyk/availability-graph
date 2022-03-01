425dd55a7eba743ddf0c0dc9f809dac535543ed3
Denys Harshanov <94528479+dflymegold@users.noreply.github.com>
2022-03-01 21:33:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,41 +17,7 @@ githublink='https://github.com/dflymegold/availability-graph'
 ########### Set up the chart
 
 
-#function = go.Figure(data=[go.Scatter(x=t, y=np.sin(t))])
-
-
-fig = go.Figure()
-
-# Add traces, one for each slider step
-for step in np.arange(0, 5, 0.1):
-        go.Scatter(
-            visible=False,
-            line=dict(color="#00CED1", width=6),
-            name="𝜈 = " + str(step),
-            x=np.arange(0, 10, 0.01),
-            y=np.sin(step * np.arange(0, 10, 0.01)))
-
-# Make 10th trace visible
-fig.data[10].visible = True
-
-# Create and add slider
-steps = []
-for i in range(len(fig.data)):
-    step = dict(
-        method="update",
-        args=[{"visible": [False] * len(fig.data)},
-              {"title": "Slider switched to step: " + str(i)}],  # layout attribute
-    )
-    step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
-    steps.append(step)
-
-sliders = [dict(
-    active=10,
-    currentvalue={"prefix": "Frequency: "},
-    pad={"t": 50},
-    steps=steps
-)]
-
+function = go.Figure(data=[go.Scatter(x=t, y=np.sin(t))])
 
 
 
@@ -66,7 +32,7 @@ app.layout = html.Div(children=[
     html.H1(myheading),
     dcc.Graph(
         id='flyingdog',
-        figure=fig
+        figure=function
     ),
     html.A('Code on Github', href=githublink),
     
