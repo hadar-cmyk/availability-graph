49e24315e16e7b335f70747710d943a06643d97e
Denys Harshanov <94528479+dflymegold@users.noreply.github.com>
2022-03-01 20:49:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,40 +4,21 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 ########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkred'
-color2='orange'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
+
+t = np.linspace(0, 10,1000)
+mytitle='Availability Graph'
+tabtitle='Graph'
+myheading='Dependency between failure and recovery rates'
+label1='hours'
+githublink='https://github.com/dflymegold/availability-graph'
+
 
 ########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
 
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
 
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+
+function = go.Figure([t,np.sin(t)],go.Layout(title))
+
 
 
 ########### Initiate the app
@@ -51,11 +32,10 @@ app.layout = html.Div(children=[
     html.H1(myheading),
     dcc.Graph(
         id='flyingdog',
-        figure=beer_fig
+        figure=function
     ),
     html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A('Data Source', href=sourceurl),
+    
     ]
 )
 
