c95b4232e35e6020ca2d69798e048fb7b1996232
Denys Harshanov <94528479+dflymegold@users.noreply.github.com>
2022-03-01 17:55:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,56 +2,25 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+from dash import dcc, html, Input, Output
 
-# Create figure
-fig = go.Figure()
-
-# Add traces, one for each slider step
-for step in np.arange(0, 5, 0.1):
-    fig.add_trace(
-        go.Scatter(
-            visible=False,
-            line=dict(color="#00CED1", width=6),
-            name="𝜈 = " + str(step),
-            x=np.arange(0, 10, 0.01),
-            y=np.sin(step * np.arange(0, 10, 0.01))))
-
-# Make 10th trace visible
-fig.data[10].visible = True
-
-# Create and add slider
-steps = []
-for i in range(len(fig.data)):
-    step = dict(
-        method="update",
-        args=[{"visible": [False] * len(fig.data)},
-              {"title": "Slider switched to step: " + str(i)}],  # layout attribute
-    )
-    step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
-    steps.append(step)
-
-sliders = [dict(
-    active=10,
-    currentvalue={"prefix": "Frequency: "},
-    pad={"t": 50},
-    steps=steps
-)]
-
-fig.update_layout(
-    sliders=sliders
-)
-
-fig.show()
-
-
-########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
 
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-########### Set up the layout
-app.layout = html.Div([dcc.Graph(figure=fig)])
+app.layout = html.Div([
+    dcc.Slider(0, 20, 5,
+               value=10,
+               id='my-slider'
+    ),
+    html.Div(id='slider-output-container')
+])
+
+@app.callback(
+    Output('slider-output-container', 'children'),
+    Input('my-slider', 'value'))
+def update_output(value):
+    return 'You have selected "{}"'.format(value)
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
