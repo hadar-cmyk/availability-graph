455620329e6f225dba4fafdd20398dbf1ac91b21
Denys Harshanov <94528479+dflymegold@users.noreply.github.com>
2022-03-02 15:06:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import numpy as np
 
 
 ########### Define your variables
-
-x=np.arange(0,5,0.1)
 mytitle = 'Availability Graph'
 tabtitle = 'Graph'
 myheading = 'Dependency between failure and recovery rates'
@@ -24,7 +22,7 @@ def lambda_arg (step_):
 def mu_arg (step_):
     return (1/step_)
 
-for step in np.arange(0,101,1):
+for step in np.arange(0,101,0.5):
     fig.add_trace(
         go.Scatter(
             visible=False,
@@ -41,14 +39,15 @@ for i in range(len(fig.data)):
     step = dict(
         method="update",
         args=[{"visible": [False] * len(fig.data)},
-              {"title": "Failure rate (failures per hour): " + str(0.1*i)}],
-        label = ''
+              {"title": "λ Failure rate (failures per hour): " + str(0.1*i)}],
+        label = str("%.2f" %(0.1*i))
     )
     step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
     steps.append(step)
 
 sliders = [dict(
     active=0,
+    currentvalue={"prefix": " λ Failure rate (failures per hour): "},
     pad={"t": 50},
     steps = steps
 )]
@@ -58,6 +57,7 @@ fig.update_layout(
     xaxis_title="Hours",
     yaxis_title="Availability",
 )
+
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
