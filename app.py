02dd3783b559b86d6b72c8a9fe3ec85b650c2eeb
Denys Harshanov <94528479+dflymegold@users.noreply.github.com>
2022-03-02 12:23:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,53 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 import plotly.graph_objs as go
 import numpy as np
 
 ########### Define your variables
 
-t = np.linspace(0, 10,10)
-mytitle='Availability Graph'
-tabtitle='Graph'
-myheading='Dependency between failure and recovery rates'
-label1='hours'
-githublink='https://github.com/dflymegold/availability-graph'
-
+x=np.arange(0,5,0.1)
+mytitle = 'Availability Graph'
+tabtitle = 'Graph'
+myheading = 'Dependency between failure and recovery rates'
+label1 = 'hours'
+githublink = 'https://github.com/dflymegold/availability-graph'
 
 ########### Set up the chart
 
 
 fig = go.Figure()
 
-for step in np.arange(0, 10, 0.1):
+def lambda_arg (step_):
+    return ((1-10)/step_)
+def mu_arg (step_):
+    return (1/step_)
+
+for step in range(1,100,1):
     fig.add_trace(
         go.Scatter(
             visible=False,
             line=dict(color="#00CED1", width=6),
             name="𝜈 = " + str(step),
-            x=t,
-            y= (10/step)/((1-100/step)+(10/step))+(1-100/step)/((1-100/step)+(10/step))*np.exp(-((1-100/step)+(10/step))*t)))
-    
-    
-fig.data[10].visible = True
+            x=x,
+            y = ((mu_arg(step)/(lambda_arg(step)+mu_arg(step)))+(lambda_arg(step)/(lambda_arg(step)+mu_arg(step)))*
+                 np.exp(-((lambda_arg(step)+mu_arg(step))*x)))))
 
+fig.data[10].visible = True
 
 steps = []
 for i in range(len(fig.data)):
     step = dict(
         method="update",
         args=[{"visible": [False] * len(fig.data)},
-              {"title": "Slider switched to step: " + str(i)}],  # layout attribute
+              {"title": "Slider switched to hour: " + str(i+1)}],  # layout attribute
     )
     step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
     steps.append(step)
 
 sliders = [dict(
-    active=10,
-    currentvalue={"prefix": "Frequency: "},
+    active=1,
+    currentvalue={"prefix": "Hours: "},
     pad={"t": 50},
     steps=steps
 )]
@@ -53,25 +56,21 @@ fig.update_layout(
     sliders=sliders
 )
 
-
-
-
-
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title=tabtitle
+app.title = tabtitle
 
 ########### Set up the layout
 app.layout = html.Div(children=[
     html.H1(myheading),
     dcc.Graph(
         id='graph',
-        figure=fig ),
+        figure=fig),
     html.Br(),
     html.A('Code on Github', href=githublink),
-    ])
+])
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
